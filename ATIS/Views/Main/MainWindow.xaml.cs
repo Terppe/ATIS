e064672dc06644398946ad67b032896ba1927de4
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-04-25 11:49:26 +0200

    MainWindow moved to Folder Views-Main

---
+++ ATIS/Views/Main/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ATIS.Views.Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ ATIS/Views/Main/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATIS.Views.Main
+{
+    class MainWindowViewModel
+    {
+    }
+}
