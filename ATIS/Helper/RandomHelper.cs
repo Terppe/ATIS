3ba8b13628b35d861bfb60800d49d5add520b81e
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-06-05 16:28:12 +0200

    RandomHelper installiert

---
+++ ATIS/Helper/RandomHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATIS.Ui.Helper
+{
+    public abstract class RandomHelper
+    {
+        public static int Randomnumber()
+        {
+            var rnum = new Random();
+            return rnum.Next(99, 999999999);
+        }
+    }
+}
