086f66198da7387da650cb5ca3a61d0a1ade2b42
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-09-03 18:49:55 +0200

    FlipViewImages in HomeView installed

--- ATIS/Views/Main/HomeView.xaml.cs
+++ ATIS/Views/Main/HomeView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using MahApps.Metro.Controls;
 
 namespace ATIS.Ui.Views.Main
 {
@@ -16,5 +17,32 @@ namespace ATIS.Ui.Views.Main
             LbVersion.Content = ".NET Core Version: " + System.Environment.Version;
 
         }
+
+        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var flipview = ((FlipView)sender);
+            switch (flipview.SelectedIndex)
+            {
+                case 0:
+                    flipview.BannerText = "saltwaterfishes!";
+                    break;
+                case 1:
+                    flipview.BannerText = "hippos!";
+                    break;
+                case 2:
+                    flipview.BannerText = "aquarium!";
+                    break;
+                case 3:
+                    flipview.BannerText = "home!";
+                    break;
+                case 4:
+                    flipview.BannerText = "private!";
+                    break;
+                case 5:
+                    flipview.BannerText = "settings!";
+                    break;
+            }
+        }
+
     }
 }
--- ATIS/Views/Main/HomeViewModel.cs
+++ ATIS/Views/Main/HomeViewModel.cs
@@ -1,8 +1,32 @@
-﻿using ATIS.Ui.Helper;
+﻿using System;
+using ATIS.Ui.Helper;
 
 namespace ATIS.Ui.Views.Main
 {
     public class HomeViewModel : ViewModelBase
     {
+        public HomeViewModel()
+        {
+            FlipViewImages = new Uri[]
+            {
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Aquarium.jpg",
+                    UriKind.RelativeOrAbsolute),
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Nilpferd.jpg",
+                    UriKind.RelativeOrAbsolute),
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Aquarium2.jpg",
+                    UriKind.RelativeOrAbsolute),
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Home.jpg",
+                    UriKind.RelativeOrAbsolute),
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Privat.jpg",
+                    UriKind.RelativeOrAbsolute),
+                new Uri("pack://application:,,,/ATIS.Ui;component/../Asset/Images/Settings.jpg",
+                    UriKind.RelativeOrAbsolute)
+            };
+        }
+
+        public Uri[] FlipViewImages { get; set; }
+
+
     }
 }
+
