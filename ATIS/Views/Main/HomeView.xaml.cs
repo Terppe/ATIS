11148b5b7e2b1c7839beccba70773ac5329f2402
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-09-06 15:12:19 +0200

    FlipView vereinfacht

--- ATIS/Views/Main/HomeView.xaml.cs
+++ ATIS/Views/Main/HomeView.xaml.cs
@@ -18,31 +18,31 @@ namespace ATIS.Ui.Views.Main
 
         }
 
-        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var flipview = ((FlipView)sender);
-            switch (flipview.SelectedIndex)
-            {
-                case 0:
-                    flipview.BannerText = "saltwaterfishes!";
-                    break;
-                case 1:
-                    flipview.BannerText = "hippos!";
-                    break;
-                case 2:
-                    flipview.BannerText = "aquarium!";
-                    break;
-                case 3:
-                    flipview.BannerText = "home!";
-                    break;
-                case 4:
-                    flipview.BannerText = "private!";
-                    break;
-                case 5:
-                    flipview.BannerText = "settings!";
-                    break;
-            }
-        }
+        //private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    var flipview = ((FlipView)sender);
+        //    switch (flipview.SelectedIndex)
+        //    {
+        //        case 0:
+        //            flipview.BannerText = "saltwaterfishes!";
+        //            break;
+        //        case 1:
+        //            flipview.BannerText = "hippos!";
+        //            break;
+        //        case 2:
+        //            flipview.BannerText = "aquarium!";
+        //            break;
+        //        case 3:
+        //            flipview.BannerText = "home!";
+        //            break;
+        //        case 4:
+        //            flipview.BannerText = "private!";
+        //            break;
+        //        case 5:
+        //            flipview.BannerText = "settings!";
+        //            break;
+        //    }
+        //}
 
     }
 }
--- ATIS/Views/Main/MainWindowViewModel.cs
+++ ATIS/Views/Main/MainWindowViewModel.cs
@@ -9,21 +9,10 @@ namespace ATIS.Ui.Views.Main
 {
     public class MainWindowViewModel : ViewModelBase
     {
-      //  private readonly ObservableCollection<ViewModelBase> _settings;
-
-        ///  ############# TreeView #############
-      //  public ObservableCollection<ViewModelBase> Settings => _settings;
-
 
         public MainWindowViewModel()
         {
-            //_settings = new ObservableCollection<ViewModelBase>();
-
-            //Settings.Add(new HomeViewModel());
-            //Settings.Add(new DatabaseViewModel());
-            //Settings.Add(new RegnumsViewModel());
-            //Settings.Add(new PhylumsViewModel());
-
+ 
         }
         private RelayCommand _getByNameOrIdCommand;
         public ICommand GetByNameCommand => _getByNameOrIdCommand ??= new RelayCommand(delegate { ExecuteGetByName(SearchName); });
