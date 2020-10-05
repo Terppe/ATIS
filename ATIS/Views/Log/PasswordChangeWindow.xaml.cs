568d2df7cc9456eb9f6e75d5c94ac768626cec37
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-10-05 17:25:39 +0200

    PasswordChange und PasswordForget hinzu

---
+++ ATIS/Views/Log/PasswordChangeWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ATIS.Ui.Views.Main;
+using MahApps.Metro.Controls;
+
+namespace ATIS.Ui.Views.Log
+{
+    /// <summary>
+    /// Interaktionslogik für PasswordChangeWindow.xaml
+    /// </summary>
+    public partial class PasswordChangeWindow : MetroWindow, MainWindowViewModel.IView
+    {
+        public PasswordChangeWindow()
+        {
+            InitializeComponent();
+        }
+
+        public PasswordChangeWindow(AuthenticationViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+
+        public IViewModel ViewModel { get; set; }
+    }
+}
---
+++ ATIS/Views/Log/PasswordForgotWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ATIS.Ui.Views.Main;
+using MahApps.Metro.Controls;
+
+namespace ATIS.Ui.Views.Log
+{
+    /// <summary>
+    /// Interaktionslogik für PasswordForgotWindow.xaml
+    /// </summary>
+    public partial class PasswordForgotWindow : MetroWindow, MainWindowViewModel.IView
+    {
+        public PasswordForgotWindow()
+        {
+            InitializeComponent();
+        }
+
+        public PasswordForgotWindow(AuthenticationViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+
+        public IViewModel ViewModel { get; set; }
+    }
+}
