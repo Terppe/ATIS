712f62592db510f3196daecbbefac8fe799311c5
Rudolf Terppe <rudolf.terppe@hotmail.de>
2020-10-17 16:45:03 +0200

    DivisionView mit Gridsplitter noch nicht ok

---
+++ ATIS/Views/Database/D15Subdivision/SubdivisionView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using ATIS.Ui.Views.Database.D09Division;
+
+namespace ATIS.Ui.Views.Database.D15Subdivision
+{
+    /// <summary>
+    /// Interaktionslogik für SubdivisionView.xaml
+    /// </summary>
+    public partial class SubdivisionView : UserControl
+    {
+        public SubdivisionView()
+        {
+            DataContext = new DivisionsViewModel();
+
+            InitializeComponent();
+            IsVisibleChanged += UserControl_IsVisibleChanged;
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                Dispatcher.BeginInvoke(
+                    DispatcherPriority.ContextIdle,
+                    new Action(delegate
+                    {
+                        TbSearchDivision.Focus();
+                    }));
+            }
+        }
+        private void TbSearchDivision_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab || e.Key == Key.Enter)
+            {
+                BtnGet.Focus();
+                e.Handled = true;
+            }
+        }
+    }
+}
