de353b880d2e89910a29e38e1be1bc0bf5634eec
Rudolf Terppe <rudolf.terppe@hotmail.de>
2021-03-05 16:46:15 +0100

    HomeView Mail and web

--- ATIS/Views/Main/HomeView.xaml.cs
+++ ATIS/Views/Main/HomeView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
 
 namespace ATIS.Ui.Views.Main
 {
@@ -43,5 +51,104 @@ namespace ATIS.Ui.Views.Main
         //    }
         //}
 
+
+        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = ConfigurationManager.AppSettings["GitHub"] ?? string.Empty,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+
+        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var body = "This is a body of a message";
+            //var recipients = String.Join(",", "rudolf@terppe.de", "marion@terppe.de");
+            //string mailto = $"mailto:{recipients}?Subject={"Subject of message"}&Body={body}";
+            //mailto = Uri.EscapeUriString(mailto);
+            //// System.Diagnostics.Process.Start(mailto);
+            //System.Diagnostics.Process.Start(new ProcessStartInfo(mailto) { UseShellExecute = true });
+            ////    Process.Start("mailto://rudolf@terppe.de");
+            System.Diagnostics.Process.Start(new ProcessStartInfo("mailto:rudolf@terppe.de") { UseShellExecute = true });
+        }
+
+        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://pledgie.com/campaigns/31029");
+        }
+
+        private void ItisUSAButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://itis.gov",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+        private void ItisCandaButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "http://www.cbif.gc.ca",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        private void CoLifeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "http://www.catalogueoflife.org/annual-checklist/2015/",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        private void EOLButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "http://www.eol.org/",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+        private void MexicoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "http://www.conabio.gob.mx/",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        private void GlobalBioButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "http://www.gbif.org/",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+        //mexico  http://www.conabio.gob.mx/
+        //Species 2000   http://www.sp2000.org/
+        //Global Bio  http://www.gbif.org/
+        //Catalog of Life  http://www.catalogueoflife.org/annual-checklist/2015/
+        //EOL  http://www.eol.org/
+
+        private void HelpLink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Hyperlink thisLink = (Hyperlink)sender;
+            string navigateUri = thisLink.NavigateUri.ToString();
+            Process.Start(new ProcessStartInfo(navigateUri));
+            e.Handled = true;
+        }
     }
 }
